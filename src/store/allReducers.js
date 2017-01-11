cad17b664eabfa0ca31be4eef4566bdc7777f831
lixiaoyang <lixiaoyang2345@gmail.com>
2017-01-11 11:42:20 +0800

    reducers

---
+++ src/store/allReducers.js
@@ -0,0 +1,3 @@
+/**
+ * Created by lixiaoyang on 2017/1/11.
+ */
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { default as allReducers } from './reducers';
+import { default as allReducers } from './combineReducers';
 
 export default function configureStore(initialState = {}) {
   // ======================================================
